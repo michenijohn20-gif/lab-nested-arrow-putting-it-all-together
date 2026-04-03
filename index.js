eb090eea6ccb9db038f34496edb50e7471e9c7dc
John Muthomi <michenijohn20@gmail.com>
2026-04-03 12:00:32 +0300

    Submission

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3; 
+  return (passwordAttempt) => {
+    if (attemptCount >= maxAttempts) {
+      return "Account locked due to too many failed login attempts.";
+    }
+    attemptCount++;
+    if (passwordAttempt === userInfo.password) {
+      return `Login successful!`;
+    } else {
+      if (attemptCount >= maxAttempts) {
+        return "Account locked due to too many failed login attempts";
+      }
+      return `Login failed.`;
+    }
+  };
+}
 
 
 module.exports = {
